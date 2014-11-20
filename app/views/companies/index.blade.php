56350b5618ba6fff356db2041db1b4ba1f968a24
eveR Vasquez <pever@unsm.edu.pe>
2014-11-20 00:46:32 -0500

    add routes companies

--- app/views/companies/index.blade.php
+++ app/views/companies/index.blade.php
@@ -4,6 +4,13 @@
 
 <div class="col-lg-12">
     <h1 class="page-header">Companies</h1>
+    <div class="row">
+        <div class="col-lg-4">
+
+        </div>
+        <div class="col-lg-8">
+        <div class="map"></div>
+        </div>
 </div>
 
 @overwrite
