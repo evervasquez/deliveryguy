d1f0daec92191a3495be6d85ee837610be8e2762
eveR Vasquez <pever@unsm.edu.pe>
2014-12-25 17:03:11 -0500

    agregado el deliveries

--- app/delivery/Delivery/DeliveryRepositorie.php
+++ app/delivery/Delivery/DeliveryRepositorie.php
@@ -47,14 +47,9 @@ class DeliveryRepositorie
         if ($delivery->save()) {
             $max = \DB::table('deliveries')->whereNull('deleted_at')->max('id');
             $delivery = \DB::table('deliveries')->where('id', '=', $max)->get();
-            return \Response::json(array(
-                "Result" => "OK",
-                "delivery" => $delivery
-            ));
+            return $delivery;
         } else {
-            return \Response::json(array(
-                "Result" => "ERROR"
-            ));
+            return null;
         }
     }
 
