8bc779757dc4cc68a0fd75ea3a52d4170e3a7dce
eveR Vasquez <pever@unsm.edu.pe>
2015-02-19 20:35:05 +0000

    facebook

--- app/domain/delivery/Employee/EmployeeRepositorie.php
+++ app/domain/delivery/Employee/EmployeeRepositorie.php
@@ -106,7 +106,40 @@ class EmployeeRepositorie extends BaseRepository implements InterfaceRepository,
 
     public function loginWithFacebook()
     {
-        // TODO: Implement loginWithFacebook() method.
+        // get data from input
+        $code = \Input::get( 'code' );
+
+        // get fb service
+        $fb = \OAuth::consumer( 'Facebook' );
+
+        // check if code is valid
+
+        // if code is provided get user data and sign in
+        if ( !empty( $code ) ) {
+
+            // This was a callback request from facebook, get the token
+            $token = $fb->requestAccessToken( $code );
+
+            // Send a request with it
+            $result = json_decode( $fb->request( '/me' ), true );
+
+            $message = 'Your unique facebook user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
+            echo $message. "<br/>";
+
+            //Var_dump
+            //display whole array().
+            dd($result);
+
+        }
+        // if not ask for permission first
+        else {
+            // get fb authorization
+            $url = $fb->getAuthorizationUri();
+
+            // return to facebook login url
+            return \Redirect::to( (string)$url );
+        }
+
     }
 
 
