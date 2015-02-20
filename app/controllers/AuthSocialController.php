99a6463711f0c60f9a0fbcc075dde53186bfd907
eveR Vasquez <pever@unsm.edu.pe>
2015-02-20 21:39:12 +0000

    create controller oauth

---
+++ app/controllers/AuthSocialController.php
@@ -0,0 +1,27 @@
+<?php
+use domain\social\GoogleManager;
+use domain\social\FacebookManager;
+
+class AuthSocialController extends \BaseController
+{
+    protected $google;
+    protected $facebook;
+
+    function __construct(GoogleManager $google, FacebookManager $facebook)
+    {
+        $this->google = $google;
+        $this->facebook = $facebook;
+    }
+
+
+    public function fbLogin()
+    {
+        return $this->facebook->loginWithFacebook();
+    }
+
+    public function fbCallback()
+    {
+        $code = Input::get('code');
+        dd($code);
+    }
+}
