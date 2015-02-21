677b0bc1c1390b60a02c55482fc5bbcf84687c18
eveR Vasquez <pever@unsm.edu.pe>
2015-02-21 01:37:35 +0000

    arreglando login facebook

--- app/domain/social/FacebookAuth.php
+++ app/domain/social/FacebookAuth.php
@@ -3,6 +3,7 @@
 namespace domain\social;
 
 use Facebook\FacebookRequest;
+use Facebook\GraphUser;
 
 class FacebookAuth implements FacebookManager
 {
@@ -16,7 +17,6 @@ class FacebookAuth implements FacebookManager
     public function loginWithFacebook()
     {
         $this->helper = new LaravelFacebookRedirectLoginHelper(route('oauth.fb.callback'));
-        $this->session = $this->helper->getSessionFromRedirect();
         return \Redirect::to($this->helper->getLoginUrl());
     }
 
@@ -31,11 +31,32 @@ class FacebookAuth implements FacebookManager
         if (strlen($code) == 0) {
             return Redirect::route('sign-up')->with('message', 'There was an error communicating with Facebook');
         }
+        $this->helper = new LaravelFacebookRedirectLoginHelper(route('oauth.fb.callback'),getenv('FACEBOOK_CLIENT_ID'),getenv('FACEBOOK_CLIENT_SECRET'));
+        try {
+            $session = $this->helper->getSessionFromRedirect();
+        } catch(FacebookSDKException $e) {
+            $session = null;
+        }
 
-        $request = new FacebookRequest($this->session, 'GET', '/me');
-        $response = $request->execute();
-        $graphObject = $response->getGraphObject();
-        return $graphObject;
+        if($session) {
+
+            try {
+
+                $user_profile = (new FacebookRequest(
+                    $session, 'GET', '/me'
+                ))->execute()->getGraphObject(GraphUser::className());
+
+                echo "Name: " . $user_profile->getName();
+
+            } catch(FacebookRequestException $e) {
+
+                echo "Exception occured, code: " . $e->getCode();
+                echo " with message: " . $e->getMessage();
+
+            }
+
+        }
+
     }
 
 
