a89f8ef29c882406bb41cd347d7b1837bb53f31f
eveR Vasquez <pever@unsm.edu.pe>
2015-02-22 05:21:27 +0000

    add register google

--- app/domain/social/GoogleAuth.php
+++ app/domain/social/GoogleAuth.php
@@ -41,13 +41,13 @@ class GoogleAuth implements GoogleLogin
     {
         if (!empty($code)) {
             // This was a callback request from google, get the token
-            $google = OAuth::consumer('Google');
+            $google = \OAuth::consumer('Google');
             $token = $google->requestAccessToken($code);
             // Send a request with it
             $result = json_decode($google->request('https://www.googleapis.com/oauth2/v1/userinfo'), true);
             return $result;
         } else {
-            return \Redirect::route('sign-up')->with('message', 'There was an error communicating with Facebook');
+            return \Redirect::route('sign-up')->with('message', 'There was an error communicating with Google');
         }
     }
 
